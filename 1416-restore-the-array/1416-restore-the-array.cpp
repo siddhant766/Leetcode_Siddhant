55af735eb9b3b4029eb59f00304c22ee896881a4
siddhant patel <152314055+siddhant766@users.noreply.github.com>
2026-09-04 20:14:01 +0530

    Time: 17 ms (91.73%), Space: 17.9 MB (80.95%) - LeetHub

---
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfArrays(string s, int k) {
+        const int MOD = 1e9 + 7;
+        int n = s.size();
+        vector<long long> dp(n+1,0);
+        dp[n] = 1;
+        for(int i=n-1;i>=0;i--){
+            if(s[i] == '0'){
+                continue;
+            }
+            long long num = 0;
+            for(int j=i;j<n;j++){
+                num = num * 10+(s[j] - '0');
+                if(num>k){
+                    break;
+                }
+                dp[i] = (dp[i] + dp[j+1] )% MOD;
+            }
+        }
+        return dp[0];
+    }
+};
